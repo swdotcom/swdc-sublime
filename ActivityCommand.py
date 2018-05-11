0c37f362a025e6b8f87b831f6bf6f7c1a85d66d5
Xavier <xavluiz@gmail.com>
2018-05-10 17:23:46 -0700

    updated the sublime plugin version

--- ActivityCommand.py
+++ ActivityCommand.py
@@ -11,7 +11,7 @@ import urllib
 import subprocess
 import sublime_plugin, sublime
 
-VERSION = '0.1.0'
+VERSION = '0.1.1'
 PM_URL = 'localhost:19234'
 USER_AGENT = 'Software.com Sublime Plugin v' + VERSION
 LOGGING = True
