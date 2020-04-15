02ae275c62c8d90dc3f0004ed3cfecbb69c7af84
brettmstevens7 <brettmstevens7@gmail.com>
2020-04-15 15:58:02 -0700

    release version 1.0.3

--- lib/SoftwareUtil.py
+++ lib/SoftwareUtil.py
@@ -19,7 +19,7 @@ from .SoftwareSettings import *
 from .SoftwareModels import Project
 
 # the plugin version
-VERSION = '1.0.2'
+VERSION = '1.0.3'
 PLUGIN_ID = 1
 
 DEFAULT_SESSION_THRESHOLD_SECONDS = 60 * 15
